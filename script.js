d331837868a362e2bb3920e4a590611dc6d005d2
hungphqu98 <pqh2405@gmail.com>
2020-12-16 18:43:43 +0700

    delete

--- script.js
+++
@@ -1,9 +0,0 @@
-var cal = new tui.Calendar('#calendar', {
-    defaultView: 'month',
-    taskView: true,
-    template: {
-      monthDayname: function(dayname) {
-        return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
-      }
-    }
-  });
